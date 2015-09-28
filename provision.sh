5c3bc159fa3e933d50584eec1e62faefc8dfe0c5
Daniel Adler <dadler1@gwdg.de>
2015-09-28 07:44:01 +0200

    if idp takes more time to get ready, sp/metadata.sh checks now in a loop for valid metadata and retries

--- provision.sh
+++ provision.sh
@@ -4,6 +4,3 @@ vagrant provision sp --provision-with base,install
 vagrant up idp 
 vagrant provision sp --provision-with metadata
 
-# mirror,base,install
-# --no-provision
-# vagrant provision idp --provision-with mirror,base,install,metadata
--- sp/metadata.sh
+++ sp/metadata.sh
@@ -1,3 +1,15 @@
-curl -ks -o /etc/shibboleth/partner-metadata.xml https://idp.example.org/idp/profile/Metadata/SAML
-service shibd restart
+while true 
+do 
+  curl -ks -o /etc/shibboleth/partner-metadata.xml https://idp.example.org/idp/profile/Metadata/SAML
+  cat /etc/shibboleth/partner-metadata.xml | grep Error >/dev/null
+  if [ $? -ne 0 ]; then
+    echo "downloaded idp metadata"
+    exit 0
+  fi
+  echo "idp metadata not available / waiting 5 secs and then retry.."
+  sleep 5
+done
+#while [ $? -eq 0 ];
+#echo $?
+# service shibd restart
 
