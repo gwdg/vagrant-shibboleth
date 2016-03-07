bfbc71891abecad47955cc178cb7f08707fd0190
Daniel Adler <dadler1@gwdg.de>
2016-03-07 14:19:21 +0100

    fixed: enabled ECP in SP

--- sp/sso.sh
+++ sp/sso.sh
@@ -5,12 +5,14 @@ case $1 in
     xmlstarlet ed -L \
       -d "/_:SPConfig/_:ApplicationDefaults/_:Sessions/_:SSO/@*" \
       -i "/_:SPConfig/_:ApplicationDefaults/_:Sessions/_:SSO" -t attr -n entityID -v "https://idp.example.org/idp/shibboleth" \
+      -i "/_:SPConfig/_:ApplicationDefaults/_:Sessions/_:SSO" -t attr -n ECP      -v "true" \
       /etc/shibboleth/shibboleth2.xml 
     ;;
   idp3)
     xmlstarlet ed -L \
       -d "/_:SPConfig/_:ApplicationDefaults/_:Sessions/_:SSO/@*" \
       -i "/_:SPConfig/_:ApplicationDefaults/_:Sessions/_:SSO" -t attr -n entityID -v "https://idp3.example.org/shibboleth" \
+      -i "/_:SPConfig/_:ApplicationDefaults/_:Sessions/_:SSO" -t attr -n ECP      -v "true" \
       /etc/shibboleth/shibboleth2.xml 
     ;;
   eds)
@@ -18,6 +20,7 @@ case $1 in
       -d "/_:SPConfig/_:ApplicationDefaults/_:Sessions/_:SSO/@*" \
       -i "/_:SPConfig/_:ApplicationDefaults/_:Sessions/_:SSO" -t attr -n discoveryProtocol -v "SAMLDS" \
       -i "/_:SPConfig/_:ApplicationDefaults/_:Sessions/_:SSO" -t attr -n discoveryURL      -v "https://sp.example.org/DS/WAYF" \
+      -i "/_:SPConfig/_:ApplicationDefaults/_:Sessions/_:SSO" -t attr -n ECP               -v "true" \
       /etc/shibboleth/shibboleth2.xml 
     ;;
   *)
